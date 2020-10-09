c958b1166616b901241c0eebff6e06bb785e41c5
AccountHyzo <AccountHyzo@hotmail.com>
2020-10-09 08:16:59 +0200

    Initial Commit

---
+++ media/lua/server/Items/Survival_Distributions.lua
@@ -0,0 +1,1193 @@
+FirearmsDistributions = Distributions or {};
+
+local distributionTable = {
+
+    burgerkitchen =
+    {
+		counter ={
+
+			items = {
+      "Firearms.AluminumBaseballBat", 0.25,
+			},
+
+			junk = {
+
+				items = {
+        "Firearms.AluminumBaseballBat", 0.5,
+				},
+			},
+		}
+	},
+
+    medclinic =
+    {
+        counter =
+        {
+
+            items = {
+            "Firearms.AluminumBaseballBat", 0.25,
+            }
+        }
+    },
+
+    medicalstorage = {
+        counter =
+        {
+
+            items = {
+                "Firearms.AluminumBaseballBat", 0.5,
+            }
+        },
+
+        metal_shelves =
+        {
+
+            items = {
+            "Firearms.AluminumBaseballBat", 0.25,
+            }
+        }
+    },
+
+    zippeestore =
+    {
+        counter = {
+
+            items = {
+            "Firearms.AluminumBaseballBat", 0.25,
+            }
+        },
+
+        shelvesmag ={
+
+            items = {
+            "Firearms.GunnutMonthly1", 2,
+            }
+        },
+
+        crate = {
+
+            items = {
+            "Firearms.AluminumBaseballBat", 4,
+            }
+        }
+    },
+
+    grocerystorage =
+    {
+        bin = {
+
+            items = {
+            "Firearms.AluminumBaseballBat", 4,
+            }
+        },
+    },
+
+
+    grocery =
+    {
+
+        counter ={
+
+            items = {
+            "Firearms.AluminumBaseballBat", 4,
+            },
+            junk = {
+
+                items = {
+
+                }
+            }
+        },
+
+        smallbox= {
+
+            items = {
+
+            }
+        },
+
+        shelvesmag ={
+
+            items = {
+
+            }
+        },
+
+        crate = {
+
+            items = {
+            "Firearms.AluminumBaseballBat", 4,
+            }
+        }
+    },
+
+    gigamart =
+    {
+
+        counter ={
+
+            items = {
+            "Firearms.AluminumBaseballBat", 4,
+            },
+            junk = {
+
+                items = {
+
+                }
+            }
+        },
+
+        shelvesmag ={
+
+            items = {
+
+            }
+        },
+
+        crate = {
+
+            items = {
+            "Firearms.AluminumBaseballBat", 4,
+            }
+        }
+    },
+
+    fossoil =
+    {
+        counter ={
+
+            items = {
+            }
+        },
+
+        shelvesmag ={
+
+            items = {
+
+            }
+        },
+
+        crate = {
+
+            items = {
+            "Firearms.AluminumBaseballBat", 4,
+            }
+        }
+    },
+
+	bedroom =
+	{
+
+        plankstash ={
+
+            items = {
+            }
+        },
+
+		sidetable = {
+
+			items = {
+			},
+
+			junk = {
+
+				items = {
+				},
+			},
+        },
+	},
+
+    loggingfactory =
+    {
+        crate = {
+
+            items = {
+            "Firearms.AluminumBaseballBat", 4,
+            }
+        },
+    },
+
+    all =
+    {
+        plankstash ={
+            items = {
+            "Firearms.AluminumBaseballBat", 4,
+            }
+        },
+
+        dresser = {
+
+            items = {
+            }
+        },
+
+        postbox = {
+
+            items = {
+            },
+        },
+
+        corn ={
+
+            items = {
+            }
+        },
+
+		locker ={
+    "Firearms.AluminumBaseballBat", 4,
+		},
+
+        inventorymale = {
+
+            items = {
+            "Firearms.AluminumBaseballBat", 4,
+            }
+        },
+        inventoryfemale = {
+
+            items = {
+            "Firearms.AluminumBaseballBat", 4,
+            }
+
+        },
+        shelvesmag ={
+
+            items = {
+            }
+        },
+
+		desk = {
+
+			items = {
+			},
+
+			junk = {
+
+				items = {
+				},
+			},
+		},
+
+		filingcabinet = {
+
+			items = {
+			},
+
+			junk = {
+
+				items = {
+				},
+			},
+		},
+
+		wardrobe = {
+		},
+
+        crate = {
+
+            items = {
+            "Firearms.AluminumBaseballBat", 4,
+            }
+        },
+
+		counter = {
+
+			items = {
+      "Firearms.AluminumBaseballBat", 4,
+			},
+
+			junk = {
+
+				items = {
+				},
+			},
+		},
+
+		sidetable = {
+
+			items = {
+			},
+
+			junk = {
+
+				items = {
+				},
+			},
+		},
+
+        bin = {
+
+            items = {
+            "Firearms.AluminumBaseballBat", 0.15,
+            }
+        },
+
+		officedrawers = {
+
+			items = {
+			},
+
+			junk = {
+
+				items = {
+				},
+			},
+		},
+
+        metal_shelves ={
+
+            items = {
+            "Firearms.AluminumBaseballBat", 4,
+            }
+        },
+
+        shelves ={
+
+            items = {
+            }
+        },
+
+        other = {
+
+            items = {
+            "Firearms.ShotgunShells_casing", 2,
+            }
+        },
+    },
+
+    shed =
+    {
+        other ={
+
+            items = {
+            "Firearms.AluminumBaseballBat", 4,
+            },
+
+            junk = {
+
+                items = {
+                },
+            },
+        },
+
+        logs = {
+
+            items = {
+            }
+        },
+    },
+
+
+    plazastore1 =
+    {
+        counter ={
+
+            items = {
+            }
+        },
+    },
+
+    garagestorage =
+    {
+        other ={
+
+            items = {
+            "Firearms.AluminumBaseballBat", 4,
+            },
+
+            junk = {
+
+                items = {
+                },
+            },
+        },
+    },
+
+    garage =
+    {
+        metal_shelves ={
+
+            items = {
+            }
+        },
+        crate = {
+
+            items = {
+            }
+        }
+    },
+
+    motelbedroom =
+    {
+        wardrobe ={
+
+            items = {
+            }
+        },
+    },
+
+    lobby =
+    {
+        counter ={
+
+            items = {
+            }
+        },
+    },
+
+    bookstore =
+    {
+
+        other =
+        {
+
+            items = {
+            }
+        }
+    },
+
+    grocers =
+    {
+
+        smallcrate={
+
+            items = {
+            "Firearms.AluminumBaseballBat", 0.4,
+            }
+        },
+
+        smallbox={
+
+            items = {
+            "Firearms.AluminumBaseballBat", 0.4,
+            }
+        },
+    },
+
+    library =
+    {
+        counter ={
+
+            items = {
+            }
+        },
+    },
+
+    toolstore =
+    {
+        shelves ={
+
+            items = {
+            "Firearms.AluminumBaseballBat", 4,
+            }
+        },
+
+        counter ={
+
+            items = {
+            "Firearms.AluminumBaseballBat", 2,
+            }
+        },
+    },
+
+    bar =
+    {
+        shelves ={
+
+            items = {
+            },
+        },
+
+        counter ={
+
+            items = {
+            },
+        },
+
+        bin = {
+
+            items = {
+            }
+        },
+    },
+
+    policestorage = {
+        locker ={
+
+            items = {
+            },
+        },
+
+        metal_shelves =
+        {
+
+            items = {
+            }
+        },
+    },
+
+    armystorage = {
+        locker ={
+
+            items = {
+            "Firearms.AluminumBaseballBat", 4,
+            },
+        },
+
+        metal_shelves =
+        {
+
+            items = {
+            "Firearms.AluminumBaseballBat", 4,
+            }
+        },
+    },
+
+    gunstore = {
+        counter ={
+
+            items = {
+            "Firearms.ColtPeacemaker", 4,
+            },
+            dontSpawnAmmo = true,
+        },
+
+        displaycase ={
+
+            items = {
+            "Firearms.ColtPeacemaker", 4,
+            },
+            dontSpawnAmmo = true,
+        },
+
+        locker ={
+
+            items = {
+            "Firearms.ColtPeacemaker", 4,
+            },
+            dontSpawnAmmo = true,
+        },
+
+        metal_shelves ={
+
+            items = {
+            "Firearms.ColtPeacemaker", 4,
+            },
+            dontSpawnAmmo = true,
+        },
+    },
+
+    gunstorestorage ={
+        all={
+
+            items = {
+            "Firearms.ColtPeacemaker", 4,
+            },
+
+            dontSpawnAmmo = true,
+        },
+    },
+
+    mechanic = {
+        wardrobe ={
+
+            items = {
+            "Firearms.ColtPeacemaker", 4,
+            }
+        },
+
+        metal_shelves ={
+
+            items = {
+            }
+        },
+    },
+
+    clothesstore = {
+        counter ={
+
+            items = {
+            },
+        },
+    },
+
+    motelroom = {
+        bin = {
+
+            items = {
+            }
+        },
+        other ={
+
+            items = {
+            },
+        },
+    },
+
+    motelroomoccupied = {
+
+        bin = {
+
+            items = {
+            }
+        },
+
+        other ={
+
+            items = {
+            },
+        },
+    },
+
+    empty = {
+        all ={
+
+            items = {
+            },
+        },
+    },
+
+    cafe = {
+        counter = {
+
+            items = {
+            },
+        },
+
+        bin = {
+
+            items = {
+            },
+        },
+    },
+
+    restaurant = {
+        counter = {
+
+            items = {
+            "Firearms.AluminumBaseballBat", 4,
+            },
+        },
+    },
+
+    post = {
+        counter = {
+
+            items = {
+            },
+        },
+    },
+
+    poststorage = {
+        all = {
+
+            items = {
+            },
+        },
+    },
+
+    generalstorestorage = {
+        other = {
+
+            items = {
+            "Firearms.AluminumBaseballBat", 4,
+            },
+        },
+    },
+
+    generalstore = {
+        other = {
+
+            items = {
+            "Firearms.AluminumBaseballBat", 4,
+            },
+        },
+    },
+
+    laundry = {
+        counter ={
+
+            items = {
+            },
+        },
+    },
+
+    furniturestore = {
+        freezer =
+        {
+            items = {
+            }
+        },
+
+        fridge ={
+            items = {
+            }
+        },
+
+        wardrobe ={
+            items = {
+            }
+        },
+
+        counter ={
+            items = {
+            },
+        },
+    },
+
+    storageunit = {
+        all ={
+
+            items = {
+            "Firearms.AluminumBaseballBat", 4,
+            },
+        },
+    },
+
+    fishingstorage= {
+        counter ={
+
+            items = {
+            "Firearms.AluminumBaseballBat", 4,
+            }
+        },
+
+        crate ={
+
+            items = {
+            "Firearms.AluminumBaseballBat", 4,
+            }
+        },
+    },
+
+    theatre = {
+        counter ={
+
+            items = {
+            },
+        },
+    },
+
+    theatrestorage = {
+        crate ={
+
+            items = {
+            },
+        },
+    },
+
+    cornerstore = {
+
+        counter ={
+
+            items = {
+            }
+        },
+
+        shelvesmag ={
+
+            items = {
+            },
+        },
+    },
+
+    housewarestore = {
+        shelves ={
+
+            items = {
+            "Firearms.AluminumBaseballBat", 4,
+            },
+        },
+    },
+
+    shoestore = {
+
+        counter ={
+
+            items = {
+            },
+        },
+    },
+
+    sportstorage = {
+        counter ={
+
+            items = {
+            },
+        },
+    },
+
+    giftstorage = {
+        crate ={
+
+            items = {
+            },
+        },
+    },
+
+    giftstore = {
+        shelves ={
+
+            items = {
+            },
+        },
+
+        counter ={
+
+            items = {
+            },
+        },
+    },
+
+    electronicsstore = {
+        counter ={
+
+            items = {
+            }
+        },
+
+        shelves ={
+
+            items = {
+            }
+        },
+    },
+
+    sewingstore = {
+        counter ={
+
+            items = {
+            }
+        },
+    },
+
+    departmentstore = {
+        counter = {
+
+            items = {
+            "Firearms.AluminumBaseballBat", 4,
+            },
+        },
+    },
+
+    hall = {
+        counter = {
+
+            items = {
+            },
+        },
+    },
+
+    gasstore = {
+
+        shelves = {
+
+            items = {
+            "Firearms.AluminumBaseballBat", 4,
+            }
+        },
+
+        counter = {
+
+            items = {
+            "Firearms.AluminumBaseballBat", 4,
+            }
+        }
+    },
+
+    gasstorage = {
+        metal_shelves = {
+
+            items = {
+            "Firearms.AluminumBaseballBat", 4,
+            }
+        },
+    },
+
+    farmstorage = {
+        all = {
+
+            items = {
+            "Firearms.AluminumBaseballBat", 4,
+            }
+        },
+    },
+
+    changeroom = {
+        locker = {
+
+            items = {
+            }
+        },
+
+        counter = {
+
+            items = {
+            }
+        },
+    },
+
+    hunting = {
+        locker = {
+
+            items = {
+            "Firearms.AluminumBaseballBat", 4,
+            }
+        },
+
+        metal_shelves ={
+
+            items = {
+            },
+        },
+
+        other = {
+
+            items = {
+            "Firearms.AluminumBaseballBat", 4,
+            }
+        },
+    },
+
+    camping = {
+
+
+        all = {
+
+            items = {
+            "Firearms.AluminumBaseballBat", 4,
+            }
+        },
+    },
+
+    Bag_WeaponBag = {
+
+        items = {
+        },
+        fillRand = 0,
+    },
+
+    Bag_SurvivorBag = {
+
+        items = {
+            "Firearms.AluminumBaseballBat", 4,
+        },
+        fillRand = 0,
+    },
+
+    Bag_WorkerBag = {
+
+        items = {
+        },
+        fillRand = 1,
+    },
+
+    Toolbox = {
+
+        items = {
+        },
+        fillRand = 1,
+    },
+
+    Garbagebag = {
+
+        items = {
+        },
+        fillRand = 2,
+    },
+
+    Carpenter = {
+        counter = {
+
+            items = {
+            }
+        },
+    },
+
+    Electrician = {
+        counter = {
+
+            items = {
+            }
+        },
+    },
+
+    ShotgunCache1 = {
+        ShotgunBox = {
+
+            items = {
+            "Firearms.ColtPeacemaker", 4,
+            }
+        },
+
+        Bag_DuffelBagTINT = {
+
+            items = {
+            "Firearms.ColtPeacemaker", 4,
+            },
+            fillRand=3,
+        },
+    },
+
+    ShotgunCache2 = {
+        ShotgunBox = {
+
+            items = {
+            "Firearms.ColtPeacemaker", 4,
+            }
+        },
+
+        counter = {
+
+            items = {
+            }
+        },
+    },
+
+    ToolsCache1 = {
+        ToolsBox = {
+
+            items = {
+            "Firearms.ColtPeacemaker", 4,
+            "Firearms.AluminumBaseballBat", 5,
+            },
+        },
+
+        counter = {
+
+            items = {
+            "Firearms.ColtPeacemaker", 4,
+            "Firearms.AluminumBaseballBat", 4,
+            },
+        },
+
+        Bag_DuffelBagTINT = {
+
+            items = {
+            "Firearms.ColtPeacemaker", 4,
+            "Firearms.AluminumBaseballBat", 4,
+            },
+            fillRand = 3,
+        },
+    },
+
+    GunCache1 = {
+        GunBox = {
+
+            items = {
+            "Firearms.ColtPeacemaker", 4,
+            },
+            dontSpawnAmmo = true,
+        },
+
+        counter = {
+
+            items = {
+            "Firearms.ColtPeacemaker", 4,
+            },
+        },
+
+        Bag_DuffelBagTINT = {
+
+            items = {
+            "Firearms.ColtPeacemaker", 4,
+            "Firearms.AluminumBaseballBat", 4,
+            },
+            fillRand = 3,
+        },
+    },
+
+    GunCache2 = {
+        GunBox = {
+
+            items = {
+            "Firearms.ColtPeacemaker", 4,
+            "Firearms.AluminumBaseballBat", 4,
+            },
+            dontSpawnAmmo = true,
+        },
+
+        Bag_DuffelBagTINT = {
+
+            items = {
+            "Firearms.ColtPeacemaker", 4,
+            "Firearms.AluminumBaseballBat", 4,
+            },
+            fillRand=3,
+        },
+    },
+
+    SurvivorCache1 = {
+
+        SurvivorCrate = {
+
+            items = {
+            "Firearms.ColtPeacemaker", 4,
+            "Firearms.AluminumBaseballBat", 4,
+            }
+        },
+    },
+
+    SurvivorCache2 = {
+
+        SurvivorCrate = {
+
+            items = {
+            "Firearms.ColtPeacemaker", 4,
+            "Firearms.AluminumBaseballBat", 4,
+            }
+        },
+    },
+
+    security =
+    {
+        locker =
+        {
+
+            items = {
+            "Firearms.ColtPeacemaker", 4,
+            },
+            dontSpawnAmmo = true,
+        },
+    },
+
+    armysurplus = {
+        shelves = {
+
+            items = {
+            "Firearms.ColtPeacemaker", 4,
+            "Firearms.AluminumBaseballBat", 4,
+            },
+        },
+    },
+
+    armyhanger = {
+        metal_shelves = {
+
+            items = {
+            "Firearms.ColtPeacemaker", 4,
+            },
+        },
+
+        counter = {
+
+            items = {
+            "Firearms.ColtPeacemaker", 4,
+            },
+        },
+
+        locker = {
+
+            items = {
+            "Firearms.ColtPeacemaker", 4,
+            },
+        },
+    },
+}
+
+table.insert(Distributions, distributionTable);
+
+--*******All Items***************--
+--"Firearms.AluminumBaseballBat"
+--"Firearms.ColtPeacemaker"
+--*****************************--
